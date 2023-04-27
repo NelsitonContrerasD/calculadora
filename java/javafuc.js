9f3092898b525d2063acc5357d7b5d418944f00d
NelsitonContrerasD <129330952+NelsitonContrerasD@users.noreply.github.com>
2023-04-27 16:02:08 -0500

    hay

--- java/javafuc.js
+++ java/javafuc.js
@@ -1,7 +1,7 @@
 
 var cal ={
-    teclas:document.querySelectorAll("#numero"),
-    operaciones:document.querySelector("#operaciones"),
+    teclas:document.querySelectorAll("#calcular li"),
+    operacion:document.querySelector("#operaciones"),
     accion:null,
     digito:null,
     cantisignos:0,
@@ -10,34 +10,69 @@ var cal ={
 }
 var me={
     inicio:function(){
-        for(var i=0;i<cal.teclas.length;i++){
+        for(var i=0; i<cal.teclas.length; i++){
             cal.teclas[i].addEventListener("click",me.oprimirteclas);
         }
     },
     oprimirteclas:function(teclas) 
     {
-        cal.accion = teclas.getAtribute("class");
+        cal.accion = teclas.getAttribute("class");
         cal.digito = teclas.target.innerHTML;
         me.calculadora(cal.accion,cal.digito);
     },
     calculadora:function(accion,digito)
     {
         switch(accion){
-            case"numero":
-            cal.cantisignos = 0;
-            if(cal.operaciones.innerHTML == "0"){
-                cal.operaciones.innerHTML = digito;
-            }
-            else{
-                if(cal.resultado){
-                    cal.resultado=false;
-                    cal.resultado.innerHTML = digito;
+            case"numero":{
+            
+                console.log("numero")
+                cal.cantisignos = 0;
+                if(cal.operacion.innerHTML == "0"){
+                    cal.operacion.innerHTML = digito;
                 }
                 else{
-                    cal.operaciones.innerHTML += digito
+                    if(cal.resultado){
+                        cal.resultado=false;
+                        cal.resultado.innerHTML = digito;
+                    }
+                    else{
+                        cal.operacion.innerHTML += digito
+                    }
                 }
-            }break;
+            }
+            break;
+            case "operador":
+            {   console.log("operador")
+                cal.cantisignos++;
+                if(cal.cantisignos ==1)
+                {
+                    if(cal.operacion.innerHTML == 0)
+                    {
+                        cal.operacion.innerHTML = 0;
+                    }else
+                    {
+                       
+                     cal.operacion.innerHTML += digito;
+                        cal.resultado = false;
+                        cal.cantidecimal=false;
+
+                    }
+                }
+            }
+            break;
+
+            case "decimal":
+            if (!cal.cantidecimal && cal.cantisignos!=1)
+            {
+                cal.operacion.innerHTML += digito;
+                cal.cantidecimal = true;
+                cal.resultado = false;
+            }
+
+            break;
         }
     }
 }
 me.inicio()
+
+
