23c62da1268c311a1ab3c0ff3cb26378f62d43fe
NelsitonContrerasD <129330952+NelsitonContrerasD@users.noreply.github.com>
2023-04-28 17:07:41 -0500

    ya suma

--- java/javafuc.js
+++ java/javafuc.js
@@ -74,14 +74,42 @@ var me={
     }
 }
 me.inicio()*/
-const pantalla = document.querySelector("#operaciones");
-const botones = document.querySelectorAll(".boton");
+const pantalla = document.querySelector(".operaciones");
+const botones = document.querySelectorAll(".btn");
 
 botones.forEach(boton => {
-    boton.addEventListener("click",()=>{
+    boton.addEventListener("click", () => {
         const botonapretado = boton.textContent;
+        if(boton.id === "c"){
 
-        pantalla.textContent = botonapretado;
+            pantalla.textContent = "0";
+            return;
+        }
+        if(boton.id === "borar" ){
+            if(pantalla.textContent.length === 1 || pantalla.textContent === "Error!"){
+                pantalla.textContent = "0";
+            }else{
+                pantalla.textContent = pantalla.textContent.slice(0, -1);
+            }
+            return;
+        }
+
+        if(boton.id === "igual"){
+            try{
+            pantalla.textContent = eval(pantalla.textContent);
+            }catch{
+                pantalla.textContent = "Error!"
+            }
+            return;
+        }
+       
+        
+        if (pantalla.textContent === "0" || pantalla.textContent === "Error!"){
+            pantalla.textContent = botonapretado;
+        }else{
+            pantalla.textContent += botonapretado;
+        }
+        
     })
 })
 
